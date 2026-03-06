9b6621f16907537729e095df9eedde0d6674fca7
deepanshu singh <deepanshusingh036@gmail.com>
2026-03-06 16:10:41 +0530

    Update rotating_search_bar.dart

--- lib/features/home/widgets/rotating_search_bar.dart
+++ lib/features/home/widgets/rotating_search_bar.dart
@@ -15,7 +15,13 @@ class RotatingSearchBar extends StatefulWidget {
 
 class _RotatingSearchBarState extends State<RotatingSearchBar>
     with SingleTickerProviderStateMixin {
-  final List<String> _keywords = ["Living Room", "Kitchen", "Office", "Bedroom", "Minimalist"];
+  final List<String> _keywords = [
+    "Living Room",
+    "Kitchen",
+    "Office",
+    "Bedroom",
+    "Minimalist",
+  ];
   late final List<String> _loopList;
 
   final TextEditingController _controller = TextEditingController();
